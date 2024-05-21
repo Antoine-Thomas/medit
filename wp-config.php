ed38ee7366b2ef3920a1a087270dcf01e715f90c
Antoine-Thomas <searching.murphy@gmail.com>
2024-05-21 15:23:47 +0200

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -1,121 +1,127 @@
 <?php
-
 /**
- * Les configurations de base de WordPress.
+ * La configuration de base de votre installation WordPress.
  *
- * Ce fichier contient les configurations suivantes : réglages MySQL, préfixe de table,
- * clés secrètes, langue de WordPress, et ABSPATH. Vous pouvez trouver plus d'informations
- * en visitant {@link http://codex.wordpress.org/Editing_wp-config.php Editing
- * wp-config.php} Codex. Vous pouvez obtenir les réglages MySQL de votre hébergeur web.
+ * Le script de création wp-config.php utilise ce fichier durant l'installation.
+ * Vous n'avez pas à utiliser le site web, vous pouvez copier ce fichier
+ * à "wp-config.php" et remplir les valeurs.
  *
- * Ce fichier est utilisé par le script de création de wp-config.php pendant l'installation.
- * Vous n'avez pas besoin d'utiliser le site web, vous pouvez juste copier ce fichier
- * dans "wp-config.php" et remplir les valeurs.
+ * Ce fichier contient les configurations suivantes :
+ *
+ * * Paramètres MySQL
+ * * Clés secrètes
+ * * Préfixe de table de base de données
+ * * ABSPATH
+ *
+ * @link https://wordpress.org/documentation/article/editing-wp-config-php/
  *
  * @package WordPress
  */
 
-/** Réglages MySQL - Vous pouvez obtenir ces informations de votre hébergeur web **/
+// ** Réglages MySQL - Vous pouvez obtenir ces informations auprès de votre hébergeur ** //
 /** Le nom de la base de données pour WordPress */
-define('DB_NAME', 'med');
+define( 'DB_NAME', 'med' );
 
-/** Nom d'utilisateur de la base de données MySQL */
-define('DB_USER', 'root');
+/** Utilisateur de la base de données MySQL */
+define( 'DB_USER', 'root' );
 
 /** Mot de passe de la base de données MySQL */
-define('DB_PASSWORD', '');
+define( 'DB_PASSWORD', '' );
 
-/** Adresse de l'hôte MySQL */
-define('DB_HOST', 'localhost');
+/** Adresse de l'hébergement MySQL */
+define( 'DB_HOST', 'localhost' );
 
-/** Jeu de caractères à utiliser pour la création des tables de la base de données. */
-define('DB_CHARSET', 'utf8');
+/** Jeu de caractères à utiliser lors de la création des tables de la base de données. */
+define( 'DB_CHARSET', 'utf8mb4' );
 
-/** Type de collation de la base de données. Ne changez pas si vous n'êtes pas sûr. */
-define('DB_COLLATE', '');
+/** Type de collation de la base de données. Ne modifiez pas cette valeur si vous n'êtes pas sûr. */
+define( 'DB_COLLATE', '' );
 
+/** Désactiver l'éditeur de fichiers intégré de WordPress */
+define( 'DISALLOW_FILE_EDIT', true );
 
+/** Définir le COOKIEHASH pour la sécurité */
+define( 'COOKIEHASH', md5('root' . 'secure cookies' . 'root') ); // Renforcement des cookies
 
-/** Désactiver WP-Cron et configurer une tâche cron réelle */
-define('DISABLE_WP_CRON', true);
+/** Définir la limite de mémoire pour WordPress */
+define( 'WP_MEMORY_LIMIT', '256M' );
 
-/** Désactiver l'éditeur de fichiers de WordPress */
-define('DISALLOW_FILE_EDIT', true);
-
-/** Paramètres de cookies sécurisés */
-define('COOKIEHASH', md5('DOCUMENT_ROOT' . 'secure cookies' . 'DOCUMENT_ROOT'));
-
-/** Augmenter la limite de mémoire PHP */
-define('WP_MEMORY_LIMIT', '256M');
-
-/** Désactiver la fonctionnalité OP cache */
+/** Désactiver la fonctionnalité OP Cache du mu-plugin */
 define('HIDE_CACHE_CLEAR', false);
 
-/** Désactiver la fonctionnalité SSO */
+/** Désactiver la fonctionnalité SSO du mu-plugin */
 define('HIDE_SSO_LINK', false);
 
 /**#@+
- * Clés uniques d'authentification et sels.
+ * Clés uniques d'authentification et salage.
  *
- * Changez-les en phrases uniques ! Vous pouvez générer ces phrases en utilisant
- * {@link https://api.wordpress.org/secret-key/1.1/salt/ le service de clé secrète de WordPress}
- * Vous pouvez les changer à tout moment pour invalider tous les cookies existants. Cela forcera
- * tous les utilisateurs à se reconnecter.
+ * Changez ces valeurs pour des phrases uniques ! Vous pouvez générer ces phrases
+ * à l'aide du {@link https://api.wordpress.org/secret-key/1.1/salt/ service de clés secrètes de WordPress}.
+ *
+ * Vous pouvez modifier ces valeurs à tout moment afin d'invalider tous les cookies existants.
+ * Cela forcera tous les utilisateurs à se reconnecter.
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'UzSL1ba=T$z%>1L|}UIMNc|U.P0JRd8zwn}ie_`M+x2k}{v0~F|h(9#@Ng4QOu)h');
-define('SECURE_AUTH_KEY',  's!iOlNwEdKOAEoWwkVsQKP^.[^wM]((-pZIbp-/B)/~*fUS7VpIiq_3?wuP.bQ$l');
-define('LOGGED_IN_KEY',    '({TcOy<bV,fv/faw~OD~nzv.9)f<d*L1&D&Whql VPdn?W2;yDN[xmoMgRhu9J&X');
-define('NONCE_KEY',        '@A9sJQpHiD_z2P W@.J@T:9f{jt/{YR6{?L~V$13Der#zpLj8RXXa>G/h3&5 A#2');
-define('AUTH_SALT',        'E-5>kj$)VDC-9D9J-oSQaW5}q&-XTxI-oO|B2BPXdg%*#Y&2Om`^8^TFjfxc;|9R');
-define('SECURE_AUTH_SALT', '~%UALv|dA6[~~UQUi)s#S`A;2#;kT_aREVrcW){=?_y4Ov<CrCmZtxTm)llfD3k');
-define('LOGGED_IN_SALT',   'UnJ.ABd&M3^j>:6#fM1}U<ugcZCA~eDwi_f:Ehp&dof@pkTi=yC9&JhNG~Q8SQBF');
-define('NONCE_SALT',       '`%KRwR~S~1ewcv^u4kakw}hukLV3PhU.`3OKNexK`^xlXecj)p|+T3(JHzX:Djn_');
+define( 'AUTH_KEY',         '~G}X]G] q[5Qk=^icv$@Ndic%I/OUu4AC1IpKG71,F2[c1NjG8#Ur8_0/~q265!/' );
+define( 'SECURE_AUTH_KEY',  'bgz p84%~yd8s&{U$Czs]|DIr1l5ue&oUJ6@NG^=!G=/_lkLe3f*|kaA:VCgn<F#' );
+define( 'LOGGED_IN_KEY',    'xRr+Q#4#rjqWULs%S=o+QwCPG3+%yv?jlW~re5}pG12/WgJq-rj,M_PAVxF037;r' );
+define( 'NONCE_KEY',        'VEP0G+iiE:B=O$ch<Aln=<b2m4:8yG.(j?<BFe|jWmo,!Kv0XiGjh}J&_|8C1Gz:' );
+define( 'AUTH_SALT',        '3csN`Yl|rh_AD$%-lY${@7PlXOI7Tv59a$0fI 25bu34V=`YZz|$mbh5@#d|QZN3' );
+define( 'SECURE_AUTH_SALT', '{q8DEK.NL>/ B=Nt,*_BO85j`^<RX?:!hOIIxR/t~s6q%M>p2<Gw!<~&Mq)p?LX]' );
+define( 'LOGGED_IN_SALT',   'dZr2,>pCjlS -p[M]Ok[nY|x1+e6j}Mi]&X,_dzRH#5deL*vEPAvd-s?[Muu+#G.' );
+define( 'NONCE_SALT',       'q2nhSJT?I=Y@W-gEZ]=MDo7clZ^S=m;9$q/5)]p4BZcjm/U0l(#z_<3^EOY{Ic_{' );
 
 /**#@-*/
 
 /**
  * Préfixe de table pour la base de données WordPress.
  *
- * Vous pouvez installer plusieurs WordPress dans une même base de données
- * en leur donnant chacune un préfixe unique. Utilisez uniquement des chiffres, lettres et underscores !
+ * Vous pouvez avoir plusieurs installations dans une seule base de données
+ * si vous donnez à chacune un préfixe unique. Utilisez uniquement des chiffres,
+ * des lettres et des underscores !
  */
 $table_prefix = 'wp_';
 
 /**
- * Langue locale de WordPress, par défaut en anglais.
- *
- * Changez cette valeur pour localiser WordPress. Un fichier MO correspondant pour
- * la langue choisie doit être installé dans wp-content/languages. Par exemple, installez
- * de_DE.mo dans wp-content/languages et définissez WPLANG à 'de_DE' pour activer
- * le support de la langue allemande.
- */
-define('WPLANG', '');
-
-/**
  * Pour les développeurs : mode de débogage de WordPress.
  *
- * Changez cette valeur à true pour activer l'affichage des notifications pendant le développement.
- * Il est fortement recommandé que les développeurs de plugins et de thèmes utilisent WP_DEBUG
- * dans leurs environnements de développement.
+ * Passez cette valeur à true pour activer l'affichage des notifications
+ * pendant le développement. Il est fortement recommandé que les développeurs
+ * de plugins et de thèmes utilisent WP_DEBUG dans leurs environnements de
+ * développement.
+ *
+ * Pour plus d'informations sur les autres constantes à utiliser pour le
+ * débogage, visitez la documentation.
+ *
+ * @link https://wordpress.org/documentation/article/debugging-in-wordpress/
  */
 define('WP_DEBUG', true);
 define('WP_DEBUG_DISPLAY', true);
 define('WP_DEBUG_LOG', true);
+$sapi_type = php_sapi_name();
+if ($sapi_type == 'cli') {
+    define('WP_DEBUG', false);
+    error_reporting(0);
+    @ini_set('display_errors', 0);
+}
+// @ini_set('log_errors', 'On');
 
-/** Définir les chemins des plugins et du contenu */
+/** Définir le répertoire des plugins mu (Must-Use) */
 define('WPMU_PLUGIN_DIR', '/mu-plugin');
+
+/** Définir le chemin du cache de Docket */
 define('DOCKET_CACHE_CONTENT_PATH', '/tmp/docket_cache');
 
 /* C'est tout, ne touchez pas à ce qui suit ! Bon blogging. */
 
 /** Chemin absolu vers le répertoire de WordPress. */
 if (!defined('ABSPATH'))
-    define('ABSPATH', dirname(__FILE__) . '/');
+	define('ABSPATH', dirname(__FILE__) . '/');
 
-/** Configure les variables de WordPress et inclut les fichiers. */
+/** Configure les variables et fichiers WordPress. */
 require_once(ABSPATH . 'wp-settings.php');
 
-/** Modifier les permissions pour l'installation de plugins */
+/** Changer les permissions pour l'installation des plugins */
 define("FS_METHOD", "direct");
+
